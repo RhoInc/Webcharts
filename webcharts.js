7fa19c651f0aca4000f30966383b70bf8bd87983
nbryant <bryant.nathana@gmail.com>
2015-05-01 15:10:04 +0100

    updating version number to 1.1.2

--- webcharts.js
+++ webcharts.js
@@ -8,7 +8,7 @@
   }
 }(this, function(d3){
 
-var webCharts = {version: "0.1.1"};
+var webCharts = {version: "0.1.2"};
 
 webCharts.colors = {
   qualitative: ['rgb(102,194,165)','rgb(252,141,98)','rgb(141,160,203)','rgb(231,138,195)','rgb(166,216,84)','rgb(255,217,47)','rgb(229,196,148)','rgb(179,179,179)'],
