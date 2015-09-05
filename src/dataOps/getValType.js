82fb1b0ea4416340ea6fa2436dc4562551bd2446
jwildfire <jwildfire@gmail.com>
2015-09-05 09:39:23 -0400

    Consider "0" numeric

--- src/dataOps/getValType.js
+++ src/dataOps/getValType.js
@@ -1,6 +1,6 @@
 export function getValType(data, variable){
 	let var_vals = d3.set(data.map(m => m[variable])).values();
-	let vals_numbers = var_vals.filter(f => +f || f === 0 );
+	let vals_numbers = var_vals.filter(f => +f || f == 0 );
 
 	if(var_vals.length === vals_numbers.length && var_vals.length > 4){
 		return 'continuous';
