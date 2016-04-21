b0c0499afb7680bc682779275ad3c79e0f3f2844
Nathan Bryant <nbryant@rhoworld.com>
2016-04-21 13:34:17 -0400

    adding function for drawing text marks; a little cleanup elsewhere

--- src/chart/updateDataMarks.js
+++ src/chart/updateDataMarks.js
@@ -1,5 +1,6 @@
-export default function (){
-  this.drawBars( this.marks.filter(f => f.type === 'bar') );
-  this.drawLines( this.marks.filter(f => f.type === 'line') );
-  this.drawPoints( this.marks.filter(f => f.type === 'circle') );
+export default function updateDataMarks() {
+  this.drawBars(this.marks.filter(f => f.type === 'bar'));
+  this.drawLines(this.marks.filter(f => f.type === 'line'));
+  this.drawPoints(this.marks.filter(f => f.type === 'circle'));
+  this.drawText(this.marks.filter(f => f.type === 'text'));
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-import d3 from 'd3';
-
 import version from './version';
 import objects from './objects';
-import {createChart} from './chart';
-import {createControls} from './controls';
-import {createTable} from './table';
+import { createChart } from './chart';
+import { createControls } from './controls';
+import { createTable } from './table';
 import multiply from './multiply';
 import dataOps from './dataOps/index';
 
 export default {
-	version: version,
-	dataOps: dataOps,
-	objects: objects,
-	createChart: createChart,
-	createControls: createControls,
-	createTable: createTable,
-	multiply: multiply
+  version,
+  dataOps,
+  objects,
+  createChart,
+  createControls,
+  createTable,
+  multiply
 };
