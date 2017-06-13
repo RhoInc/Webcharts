24b894fed0b0f894d304d8ff6649ece53414f7f0
Jeremy Wildfire <jwildfire@gmail.com>
2017-06-13 10:07:40 -0700

    uncomment tests

--- test/chart/runTests.js
+++ test/chart/runTests.js
@@ -1,19 +1,19 @@
-//import testCreateChart from './createChart';
-//
-//   import createChartSettings from '../samples/irisSettings';
-//   testCreateChart(createChartSettings);
-//
-//import testInit from './init';
-//
-//   import initSettings from '../samples/irisSettings';
-//   import initData from '../samples/irisData';
-//   testInit(initSettings,initData);
-//
-// import testRendering from './rendering';
-//
-//   import renderSettings from '../samples/element-settings';
-//   import renderData from '../samples/data';
-//   testRendering(renderSettings, renderData)
+import testCreateChart from './createChart';
+
+  import createChartSettings from '../samples/irisSettings';
+  testCreateChart(createChartSettings);
+
+import testInit from './init';
+
+  import initSettings from '../samples/irisSettings';
+  import initData from '../samples/irisData';
+  testInit(initSettings,initData);
+
+import testRendering from './rendering';
+
+  import renderSettings from '../samples/element-settings';
+  import renderData from '../samples/data';
+  testRendering(renderSettings, renderData)
 
 
 import testCheckRequired from './checkRequired';
@@ -22,14 +22,14 @@ import testCheckRequired from './checkRequired';
    import checkRequiredData from '../samples/irisData';
    testCheckRequired(checkRequiredSettings,checkRequiredData);
 
-//import testLayout from './layout';
-//
-//   import layoutSettings from '../samples/irisSettings';
-//   import layoutData from '../samples/irisData';
-//   testLayout(layoutSettings,layoutData);
-//
-//import testSetColorScale from './setColorScale';
-//
-//   import setColorScaleSettings from '../samples/irisSettings';
-//   import setColorScaleData from '../samples/irisData';
-//   testSetColorScale(setColorScaleSettings,setColorScaleData);
+import testLayout from './layout';
+
+  import layoutSettings from '../samples/irisSettings';
+  import layoutData from '../samples/irisData';
+  testLayout(layoutSettings,layoutData);
+
+import testSetColorScale from './setColorScale';
+
+  import setColorScaleSettings from '../samples/irisSettings';
+  import setColorScaleData from '../samples/irisData';
+  testSetColorScale(setColorScaleSettings,setColorScaleData);
