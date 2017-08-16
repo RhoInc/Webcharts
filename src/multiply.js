98f1db51f1fec25ab7b4bafe8ea799b9f0491ea6
jwildfire <jwildfire@rhoworld.com>
2017-08-16 09:09:40 -0700

    Link multiples back to master chart

--- src/multiply.js
+++ src/multiply.js
@@ -5,7 +5,8 @@ export default function multiply(chart, data, split_by, order) {
     let config = chart.config;
     let wrap = chart.wrap.classed('wc-layout wc-small-multiples', true).classed('wc-chart', false);
     let master_legend = wrap.append('ul').attr('class', 'legend');
-
+    chart.multiples = []
+    
     function goAhead(data) {
         let split_vals = set(data.map(m => m[split_by])).values().filter(f => f);
         if (order) {
@@ -19,6 +20,8 @@ export default function multiply(chart, data, split_by, order) {
             mchart.filters.unshift({ col: split_by, val: e, choices: split_vals });
             mchart.wrap.insert('span', 'svg').attr('class', 'wc-chart-title').text(e);
             mchart.init(data);
+            mchart.parent = chart;
+            chart.multiples.push(mchart);
         });
     }
 
