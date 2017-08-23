be79851ad127fe863ad28acfee2e9a04d82ebdf8
Jeremy Wildfire <jwildfire@gmail.com>
2017-08-23 07:25:51 -0700

    update version

--- build/webcharts.js
+++ build/webcharts.js
@@ -4,7 +4,7 @@
 	(global.webCharts = factory(global.d3));
 }(this, (function (d3) { 'use strict';
 
-var version = '1.8.1';
+var version = '1.8.2';
 
 function checkRequired(data) {
     var _this = this;
