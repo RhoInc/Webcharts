610013423c641f676ca7369a79fdd9a852a69e68
Spencer Childress <samussiah@gmail.com>
2017-10-03 15:17:04 -0400

    use column labels rather than column names in sort buttons

--- src/table/sortable/onClick.js
+++ src/table/sortable/onClick.js
@@ -17,7 +17,7 @@ export default function onClick(th, header) {
                 .append('div')
                 .datum({ key: col })
                 .classed('sort-box', true)
-                .text(col)
+                .text(header)
         };
         sortItem.wrap.append('span').classed('sort-direction', true).html('&darr;');
         sortItem.wrap.append('span').classed('remove-sort', true).html('&#10060;');
