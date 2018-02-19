94e0516a01528ebc186cca620c96055a8cd75b17
samussiah <samussiah@gmail.com>
2018-02-19 11:57:26 -0500

    allow for unspecified color_dom

--- src/chart/setColorScale.js
+++ src/chart/setColorScale.js
@@ -2,19 +2,17 @@ import naturalSorter from '../dataOps/naturalSorter';
 import { set, ascending, scale } from 'd3';
 
 export default function setColorScale() {
-    let config = this.config;
-    let data = config.legend.behavior === 'flex' ? this.filtered_data : this.raw_data;
-    let colordom =
-        config.color_dom.slice() ||
-        set(data.map(m => m[config.color_by])).values().filter(f => f && f !== 'undefined');
+    const config = this.config;
+    const data = config.legend.behavior === 'flex' ? this.filtered_data : this.raw_data;
+    const colordom = Array.isArray(config.color_dom) && config.color_dom.length
+        ? config.color_dom.slice()
+        : set(data.map(m => m[config.color_by])).values().filter(f => f && f !== 'undefined');
 
-    if (config.legend.order) {
-        colordom = colordom.sort((a, b) =>
+    if (config.legend.order)
+        colordom.sort((a, b) =>
             ascending(config.legend.order.indexOf(a), config.legend.order.indexOf(b))
         );
-    } else {
-        colordom = colordom.sort(naturalSorter);
-    }
+    else colordom.sort(naturalSorter);
 
     this.colorScale = scale.ordinal().domain(colordom).range(config.colors);
 }
