d0334e48aa69be2d45de5154582636ec0847323e
samussiah <samussiah@gmail.com>
2018-05-16 11:22:46 -0400

    point test to the right place

--- test/chart/runTests.js
+++ test/chart/runTests.js
@@ -52,9 +52,9 @@ import cleanDataData from '../samples/irisData';
 testCleanData(cleanDataSettings, cleanDataData);
 
 //test range band settings
-import testRangeBand from './chart/rangeBand';
-import { ordinal_ordinal as ordinal_ordinal_rb } from './samples/irisSettings';
-import rangeBandData from './samples/irisData';
+import testRangeBand from './rangeBand';
+import { ordinal_ordinal as ordinal_ordinal_rb } from '../samples/irisSettings';
+import rangeBandData from '../samples/irisData';
 testRangeBand(
     ordinal_ordinal_rb,
     rangeBandData
