8da95e69633043c7edaff1a9dce41491a8e5769c
Preston Burns <Preston_Burns@rhoworld.com>
2018-08-28 15:21:45 -0400

    change version and remove dynamic layout so i can see what it does in vis test

--- build/webcharts.js
+++ build/webcharts.js
@@ -6,7 +6,7 @@
           : (global.webCharts = factory(global.d3));
 })(this, function(d3) {
     'use strict';
-    var version = '1.11.0';
+    var version = '1.11.1';
 
     function init(data) {
         var _this = this;
@@ -3366,53 +3366,7 @@
             });
     }
 
-    function dynamicLayout() {
-        var widths = {
-            table: this.table.select('thead').node().offsetWidth,
-            top:
-                this.wrap.select('.table-top .searchable-container').node().offsetWidth +
-                    this.wrap.select('.table-top .sortable-container').node().offsetWidth,
-            bottom:
-                this.wrap.select('.table-bottom .pagination-container').node().offsetWidth +
-                    this.wrap.select('.table-bottom .exportable-container').node().offsetWidth
-        };
-
-        if (
-            widths.table < Math.max(widths.top, widths.bottom) &&
-            this.config.layout === 'horizontal'
-        ) {
-            this.config.layout = 'vertical';
-            this.wrap
-                .style('display', 'inline-block')
-                .selectAll('.table-top,.table-bottom')
-                .style('display', 'inline-block')
-                .selectAll('.interactivity')
-                .style({
-                    display: 'block',
-                    clear: 'both'
-                });
-        } else if (
-            widths.table >= Math.max(widths.top, widths.bottom) &&
-            this.config.layout === 'vertical'
-        ) {
-            this.config.layout = 'horizontal';
-            this.wrap
-                .style('display', 'table')
-                .selectAll('.table-top,.table-bottom')
-                .style('display', 'block')
-                .selectAll('.interactivity')
-                .style({
-                    display: 'inline-block',
-                    float: function float() {
-                        return select(this).classed('searchable-container') ||
-                            select(this).classed('pagination-container')
-                            ? 'right'
-                            : null;
-                    },
-                    clear: null
-                });
-        }
-    }
+    function dynamicLayout() {}
 
     function draw$1(passed_data) {
         var _this = this;
