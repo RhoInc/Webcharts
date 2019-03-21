b5cf44f1da6ba9f8003c3d8704d3a67784725b17
samussiah <samussiah@gmail.com>
2019-03-21 12:30:15 -0400

    apply consistent test page styling

--- test-page/createTable/index.js
+++ test-page/createTable/index.js
@@ -1,80 +1,96 @@
-var settings = {
-  "sortable":true,
-  "searchable":true,
-  "exportable":true,
-  "exports": ['csv', 'xlsx'],
-  "pagination":true,
-  "nRowsPerPage":10,
-  "nPageLinksDisplayed":5
-};
+function createTable(element, settings) {
+    var controls = webCharts.createControls(
+        element,
+        {
+            inputs: [
+                {type: 'subsetter', value_col: 'Period', label: 'Filter by Period'},
+                {type: 'subsetter', value_col: 'Group', label: 'Filter by Group'}
+            ]
+        }
+    );
 
-var controls = webCharts.createControls(".chart",	{
-		location: 'top',
-		inputs:[
-      {type: "subsetter", value_col: "Period", label: "Filter by Period"},
-      {type: "subsetter", value_col: "Group", label: "Filter by Group"}
-		]
-	})
+    return webCharts.createTable(element, settings, controls);
+}
 
-d3.csv('https://cdn.jsdelivr.net/gh/RhoInc/data-library/data/miscellaneous/elements.csv', function(error, data) {
-  var table = webCharts.createTable(".chart", settings,controls);
+var table = createTable(
+    '.table',
+    {
+        'sortable': true,
+        'searchable': true,
+        'nRowsPerPage': 10,
+        'nPageLinksDisplayed': 5,
+        'pagination': true,
+        'exportable': true,
+        'exports': ['csv', 'xlsx'],
+        'applyCSS': true,
+    }
+);
 
-  //Randomize columns.
-    d3.select('button.randomize-columns')
-        .on('click', function() {
-            table.config.cols = Object.keys(data[0])
-                .reverse()
-                .filter(function(d) {
-                    return Math.random() >= .5;
-                });
-            table.config.headers = table.config.cols;
-            console.log(table.config.cols);
-            table.draw(data);
-        });
+d3.csv(
+    'https://cdn.jsdelivr.net/gh/RhoInc/data-library/data/miscellaneous/elements.csv',
+    function(d) {
+        return d;
+    },
+    function(data) {
+        table.init(data);
 
-  //Randomize headers.
-    d3.select('button.randomize-headers')
-        .on('click', function() {
-            table.config.headers = table.config.cols
-                .map(function(key) {
-                    const
-                        strArr = [];
+        //Update settings.
+        d3.selectAll('.controls input')
+            .on('change',function(){
+                var settings = {
+                    sortable: d3.select('input.sortable').property('checked'),
+                    searchable: d3.select('input.searchable').property('checked'),
+                    nRowsPerPage: +d3.select('input.items').node().value,
+                    nPageLinksDisplayed: +d3.select('input.pages').node().value,
+                    pagination: d3.select('input.pagination').property('checked'),
+                    exportable: d3.select('input.exportable').property('checked'),
+                    applyCSS: d3.select('input.applyCSS').property('checked'),
+                };
 
-                    for (let i = 0; i < key.length; i++) {
-                        strArr.push(
-                            Math.random() >= .5
-                                ? key.substring(i,i+1).toUpperCase()
-                                : key.substring(i,i+1).toLowerCase());
-                    }
+                console.log(settings);
 
-                    return strArr.join('');
-                });
-            console.log(table.config.headers);
-            table.draw(data);
-        });
+                d3.select('.table').selectAll('*').remove()
+                var table = createTable(
+                    '.table',
+                    settings
+                );
+                table.init(data);
+            });
+    }
+);
 
-  table.init(data);
+//Randomize columns.
+d3.select('button.randomize-columns')
+    .on('click', function() {
+        var table = d3.select('.wc-table').datum();
+        table.config.cols = Object.keys(table.data.raw[0])
+            .reverse()
+            .filter(function(d) {
+                return Math.random() >= .5;
+            });
+        table.config.headers = table.config.cols;
+        console.log(table.config.cols);
+        table.draw();
+    });
 
-  d3.selectAll(".controls input").on("change",function(){
-    settings.sortable = d3.select("input.sortable").property("checked")
-    settings.searchable = d3.select("input.searchable").property("checked")
-    settings.exportable = d3.select("input.exportable").property("checked")
-    settings.pagination = d3.select("input.pagination").property("checked")
-    settings.nRowsPerPage = +d3.select("input.items").node().value
-    settings.nPageLinksDisplayed = +d3.select("input.pages").node().value
-    settings.applyCSS = d3.select("input.applyCSS").property("checked")
+//Randomize headers.
+d3.select('button.randomize-headers')
+    .on('click', function() {
+        var table = d3.select('.wc-table').datum();
+        table.config.headers = table.config.cols
+            .map(function(key) {
+                const strArr = [];
 
-    console.log(settings)
+                for (let i = 0; i < key.length; i++) {
+                    strArr.push(
+                        Math.random() >= .5
+                            ? key.substring(i,i+1).toUpperCase()
+                            : key.substring(i,i+1).toLowerCase()
+                    );
+                }
 
-    d3.select(".chart").selectAll("*").remove()
-    var controls = webCharts.createControls(".chart",	{
-    		location: 'top',
-    		inputs:[
-          {type: "subsetter", value_col: "Period", label: "Filter by Period"},
-          {type: "subsetter", value_col: "Group", label: "Filter by Group"}
-    		]
-    	})
-    var table = webCharts.createTable(".chart", settings, controls);
-    table.init(data);
-  })
-})
+                return strArr.join('');
+            });
+        console.log(table.config.headers);
+        table.draw();
+    });
